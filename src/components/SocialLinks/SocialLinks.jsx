2e4d76e250852549ac6f0b7f2baae11fdd96918b
densavi <d.savitsky93@gmail.com>
2025-10-09 23:51:15 +0300

    fix home page

--- src/components/SocialLinks/SocialLinks.jsx
+++ src/components/SocialLinks/SocialLinks.jsx
@@ -1,14 +1,17 @@
 import styles from "./SocialLinks.module.css";
 
 import Link from "next/link";
-import Image from "next/image";
+
+import InstagramIcon from '@/icons/InstagramIcon';
+import FbIcon from '@/icons/fbIcon.jsx';
+import TikTokIcon from '@/icons/TikTokIcon.jsx';
 
 export default function SocialLinks() {
 
     const socialLinks = [
-        { href: "https://instagram.com/", icon: "/images/instagram.svg", iconWidth: 19, iconHeight: 19 },
-        { href: "https://fb.com/", icon: "/images/fb.svg", iconWidth: 10, iconHeight: 18 },
-        { href: "https://www.tiktok.com/", icon: "/images/tiktok.svg", iconWidth: 19, iconHeight: 19 },
+        { href: "https://instagram.com/", icon: <InstagramIcon /> },
+        { href: "https://fb.com/", icon: <FbIcon /> },
+        { href: "https://www.tiktok.com/", icon: <TikTokIcon /> },
     ]
 
     return (
@@ -16,7 +19,7 @@ export default function SocialLinks() {
             {socialLinks.map((link) => (
                 <li key={link.href}>
                     <Link href={link.href} className={styles.socialLink}>
-                        <Image src={link.icon} alt="Social Link 1" width={link.iconWidth} height={link.iconHeight} />
+                        {link.icon}
                     </Link>
                 </li>
             ))}
